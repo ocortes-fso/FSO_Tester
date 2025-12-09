bfe3ba84b7575d05ff6c6e54b77ee0c05ee98460
Sean Fidock <sean.fidock@freespaceoperations.com.au>
2025-12-10 10:30:54 +1100

    Rear LED switch plate test costupdate

--- Subcodes/Rear_switch_plate_test.py
+++ Subcodes/Rear_switch_plate_test.py
@@ -5,43 +5,55 @@ import RPi.GPIO as GPIO
 RED_PIN = 0
 GREEN_PIN = 1
 BLUE_PIN = 7
+SWITCH_PIN = 25
+LED_SWITCH_PIN = 42
 
 
 # Initialize and setup GPIO
 GPIO.setmode(GPIO.BCM)
+
+#RGB LED pins
 GPIO.setup([RED_PIN, GREEN_PIN, BLUE_PIN], GPIO.OUT)
 
-# PWM for brightness control
-red_pwm = GPIO.PWM(RED_PIN, 1000)
-green_pwm = GPIO.PWM(GREEN_PIN, 1000)
-blue_pwm = GPIO.PWM(BLUE_PIN, 1000)
 
-red_pwm.start(0)
-green_pwm.start(0)
-blue_pwm.start(0)
-
-def set_color(r, g, b):
-    red_pwm.ChangeDutyCycle(r)
-    green_pwm.ChangeDutyCycle(g)
-    blue_pwm.ChangeDutyCycle(b)
-
-colours = [(100, 0, 0), (0, 100, 0), (0, 0, 100)] 
-delay = 1
-
-try:
-    while True:
-        for r, g, b in colours:
-            set_color(r, g, b)
-            time.sleep(delay)
-except KeyboardInterrupt:
-    pass
-finally:
-    red_pwm.stop()
-    green_pwm.stop()
-    blue_pwm.stop()
-    GPIO.cleanup()
+#Switch pins
+GPIO.setup([SWITCH_PIN, LED_SWITCH_PIN], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED_SWITCH_PIN, GPIO.OUT)
 
 
-#Switch 
+#Main Logic
 
+def set_rgb_colour(r, g, b):
+    GPIO.output(RED_PIN, r)
+    GPIO.output(GREEN_PIN, g)
+    GPIO.output(BLUE_PIN, b)
 
+def cycle_rgb():
+    colours = [
+        (1,0,0),  # Red
+        (0,1,0),  # Green   
+        (0,0,1),  # Blue
+    ]
+    
+
+while True:
+    
+    #Switch off - power button LED off, status LED cycles RGB  
+    colours = [0, 0, 0]
+    n = 0
+    if GPIO.input(SWITCH_PIN) == GPIO.low:
+        GPIO.output(LED_SWITCH_PIN, GPIO.low)
+        for colour in colours:
+            set_rgb_colour(colours[n])
+            time.sleep(1)
+            n = n + 1 
+        n = 0  
+        
+        #Switch on - power button LED on, status LED blinks RED
+    elif GPIO.input(SWITCH_PIN) == GPIO.high:
+         GPIO.output(LED_SWITCH_PIN, GPIO.high)
+         while GPIO.input(SWITCH_PIN) == GPIO.high:
+            set_rgb_colour(0, 0, 0 )
+            time.sleep(0.5)
+            set_rgb_colour(colours[0])
+            time.sleep(0.5) 
