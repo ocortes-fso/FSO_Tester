6522b3aa124d430372deb96f8e6d2bf758b15380
ocortes-fso <oscar.cortes@freespaceoperations.com.au>
2026-01-27 15:53:53 +1100

    Update Lidar.py

--- Subcodes/Lidar.py
+++ Subcodes/Lidar.py
@@ -14,9 +14,13 @@ DIST_REG = 0x00           # Need to confirm this, 00, 10, and 20 are common
 ### Show distance read by the LIDAR, below changed from older commit to allow for GUI use --removed while true and return distance value
 def read_lidar_distance():
     with SMBus(I2C_BUS) as bus:
+        try:
             data = bus.read_i2c_block_data(ADDR, DIST_REG, 2)
 
             # Big-endian (most common)
             dist = (data[0] << 8) | data[1] #mm output 
             dist_m = dist / 1000.0  # Convert to meters
             return dist_m
+
+        except OSError:
+            return None
