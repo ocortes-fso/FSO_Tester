dea65d31bde3c0f00e5d28b9b5c64386dcb981a0
Sean Fidock <sean.fidock@freespaceoperations.com.au>
2026-01-28 10:13:23 +1100

    addde ping test to GUI

--- Subcodes/Network_test.py
+++ Subcodes/Network_test.py
@@ -1,8 +1,6 @@
 # Test network connection of H16P via simple ping, and also fetch single video frame from MIPI cam
 import time
 import subprocess
-import cv2
-from PIL import Image
 
 H16P_IP = "192.168.144.10" 
 Num_pings = "10" 
@@ -15,23 +13,8 @@ fpv = None
 def ping():
     result  = subprocess.run(['ping', '-c', Num_pings, H16P_IP], stdout=subprocess.PIPE)
     if result.returncode == 0:
-        print("PASS! Network Test Passed: H16Pro receiver is reachable.")
+        time.sleep(2)
         return True
     else:
-        print("Network Test Failed: H16Pro reciever is not reachable.")
+        time.sleep(2)
         return False
-
-def cam():
-    global fpv
-    # Initialize if not already open
-    if fpv is None or not fpv.isOpened():
-        fpv = cv2.VideoCapture(RTSP)
-    
-    if fpv.isOpened():
-        ret, frame = fpv.read()
-        if ret and frame is not None:
-            return Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    else:
-        # If open fails return none, rety is handled through the GUI
-        fpv = None
-    return None
