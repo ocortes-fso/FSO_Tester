0dd81a8a38152967fea18f7c21ea30fd7a28acd7
Sean Fidock <sean.fidock@freespaceoperations.com.au>
2026-01-28 16:03:43 +1100

    fixed serial port  rate - wokring

--- Subcodes/Body_Serial_test.py
+++ Subcodes/Body_Serial_test.py
@@ -4,7 +4,7 @@ from pymavlink import mavutil
 #establish seral connection to autopilot via DSUB 
 
 serial_port = "/dev/ttyAMA0" #GPIO pin serial port may need to config boot config file to enable
-baud_rate = 57600 #again confirm this is correct serial 5 baud rate
+baud_rate = 115200 #again confirm this is correct serial 5 baud rate
 
 #test for serial receive
 
