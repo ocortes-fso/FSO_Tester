7350bb93e3a5a6521f5d538512868f1b60715dc4
Sean Fidock <sean.fidock@freespaceoperations.com.au>
2026-01-30 14:21:33 +1100

    changed serial to debug

--- Subcodes/Body_Serial_test.py
+++ Subcodes/Body_Serial_test.py
@@ -3,7 +3,7 @@ from pymavlink import mavutil
 
 #establish seral connection to autopilot via DSUB 
 
-serial_port = "/dev/ttyAMA0" #GPIO pin serial port may need to config boot config file to enable
+serial_port = "/dev/ttyAMA10" #GPIO pin serial port may need to config boot config file to enable
 baud_rate = 115200 #again confirm this is correct serial 5 baud rate
 
 #test for serial receive
