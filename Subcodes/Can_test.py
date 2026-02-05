86bdbb2166e090ab9d4d269e84a8983ba4b5625c
Sean Fidock <sean.fidock@freespaceoperations.com.au>
2026-02-05 12:22:16 +1100

    changes to can test for GUI

--- Subcodes/Can_test.py
+++ Subcodes/Can_test.py
@@ -2,33 +2,34 @@ import can
 import time
 import os
 
-CAN_INTERFACE = 'can0'  # Interface name (adjust if needed)
-BITRATE = 1000000  # Ensure this matches the flight controller's CAN bitrate
-TIMEOUT = 5  # Time in seconds to wait for a response
+def run_can():
+    CAN_INTERFACE = 'can0'  # Interface name (adjust if needed)
+    BITRATE = 1000000  # Ensure this matches the flight controller's CAN bitrate
+    TIMEOUT = 5  # Time in seconds to wait for a response
+    #Set up the CAN interface (ensure CAN bus is running)
+    os.system(f"sudo ip link set {CAN_INTERFACE} down")  # Bring CAN interface down
+    os.system(f"sudo ip link set {CAN_INTERFACE} up type can bitrate {BITRATE}")  # Set bitrate and bring CAN interface up
+    time.sleep(0.5)
 
-#Set up the CAN interface (ensure CAN bus is running)
-os.system(f"sudo ip link set {CAN_INTERFACE} down")  # Bring CAN interface down
-os.system(f"sudo ip link set {CAN_INTERFACE} up type can bitrate {BITRATE}")  # Set bitrate and bring CAN interface up
-time.sleep(0.5)
+    #Initialize CAN bus interface (debugging can remove this when all wokring)
 
-#Initialize CAN bus interface (debugging can remove this when all wokring)
-try:
     canbus = can.interface.Bus(channel=CAN_INTERFACE, bustype='socketcan')
-    print(f"CAN interface {CAN_INTERFACE} set up with bitrate {BITRATE}")
-except can.CanError:
-    print(f"Error: CAN interface {CAN_INTERFACE} is not working properly.")
-    exit(1)
 
-#Listen for CAN messages
-print(f"Listening for CAN messages on {CAN_INTERFACE}...")
-msg_rx = canbus.recv(timeout=TIMEOUT)  # Wait up to 5 seconds for a message
+    msg_rx = canbus.recv(timeout=TIMEOUT)  # Wait up to 5 seconds for a message
 
-#Check if any messages were received and print
-if msg_rx:
-    print(f"CAN check PASS!! Received message: {msg_rx}")
-else:
-    print("CAN check FAIL: No message received")
-    
-# Shut down bus 
-os.system(f"sudo ip link set {CAN_INTERFACE} down")  # Bring CAN interface down
-canbus.shutdown()
+        #Check if any messages were received and print
+    status =  None
+
+    if msg_rx:
+        status = True
+    else:
+        print("CAN check FAIL: No message received")
+        status = False
+
+    return status
+
+    # Shut down bus 
+    os.system(f"sudo ip link set {CAN_INTERFACE} down")  # Bring CAN interface down
+    canbus.shutdown()
+
+
