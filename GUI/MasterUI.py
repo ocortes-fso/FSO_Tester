e88e07d28a1d41f3709ee1e5b292c700a1608ba8
SF <sean.fidock@freespaceoperations.com.au>
2026-06-16 11:40:29 +1000

    fix error bootstyle to danger analog port test

--- GUI/MasterUI.py
+++ GUI/MasterUI.py
@@ -445,7 +445,7 @@ def body_test():
     if (0.7 <= output[0] <= 0.9) and (1.5 <= output[1] <= 1.7):
         la.after(0, lambda: la.config(text=f"PASS -- {combined_output}", bootstyle=SUCCESS, font=(None, 14)))
     if (output[0] == -1) or (output[1] == -1):
-        la.after(0, lambda: la.config(text=f"FAIL -- COULDNT WRITE PARAMS - CHECK CFG", bootstyle=SUCCESS, font=(None, 14)))
+        la.after(0, lambda: la.config(text=f"FAIL -- COULDNT WRITE PARAMS - CHECK CFG", bootstyle=DANGER, font=(None, 14)))
     else:
         la.after(0, lambda: la.config(text=f"FAIL -- {combined_output}", bootstyle=DANGER, font=(None, 14)))
 
